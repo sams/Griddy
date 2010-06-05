5202454db0eaec1146f0c9117f2a0326556c2967
Sam Sherlock <sam.sherlock@gmail.com>
2010-06-05 01:48:57 +0100

    first stage of bookmarklet

---
+++ griddy-bookmarklet.js
@@ -0,0 +1,36 @@
+/*
+ * jQuery Bookmarklet - version 1.0
+ * Originally written by: Brett Barros
+ * With modifications by: Paul Irish
+ *
+ * If you use this script, please link back to the source
+ *
+ * Copyright (c) 2010 Latent Motion (http://latentmotion.com/how-to-create-a-jquery-bookmarklet/)
+ * Released under the Creative Commons Attribution 3.0 Unported License,
+ * as defined here: http://creativecommons.org/licenses/by/3.0/
+ *
+ */
+ 
+window.bookmarklet = function(opts){fullFunc(opts)};
+ 
+// These are the styles, scripts and callbacks we include in our bookmarklet:
+window.bookmarklet({
+ 
+    css : ['http://sams.github.com/Griddy/griddy.css'],
+    js  : ['http://sams.github.com/Griddy/griddy.js'],
+    ready : function(){
+		
+			$('body').griddy({
+				rows: 6,
+				rowheight: 0,
+				rowgutter: 20,
+				columns: 6,
+				columnheight: 0,
+				columngutter: 20,
+				color: '#c3c3c3', 
+				opacity: 5
+			});
+	}
+})
+ 
+function fullFunc(a){function d(b){if(b.length===0){a.ready();return false}$.getScript(b[0],function(){d(b.slice(1))})}function e(b){$.each(b,function(c,f){$("<link>").attr({href:f,rel:"stylesheet"}).appendTo("head")})}a.jqpath=a.jqpath||"http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js";(function(b){var c=document.createElement("script");c.type="text/javascript";c.src=b;c.onload=function(){e(a.css);d(a.js)};document.body.appendChild(c)})(a.jqpath)};
